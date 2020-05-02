41022ecf90175ed7afc23f3acb170662f34e2e0a
Robert Williams <robert@a3marketing.com>
2020-05-02 00:04:46 -0500

    got adding burger functionality working

--- assets/js/burgers.js
+++ assets/js/burgers.js
@@ -4,8 +4,21 @@ console.log("Ready!")
 $("#addBurgerBtn").on("click", function(event){
     event.preventDefault();
 
-    console.log("this works");
-})
+    let addedBurger = {
+        burger_name: $("#newBurger").val().trim()
+    };
+
+$.ajax("/api/burgers", {
+    type: "POST",
+    data: addedBurger
+  }).then(
+    function() {
+      console.log("Burger Added!");
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+});
 
 
 
