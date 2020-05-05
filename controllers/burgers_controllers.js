e5d004bb1069f188e1dcc99a0091febe6a6adce1
Robert Williams <robert@a3marketing.com>
2020-05-04 21:51:14 -0500

    added route to always render index

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,7 +5,15 @@ const router = express.Router();
 const burger = require("../model/burgers");
 
 
-
+router.get("*", function(req, res){
+    burger.all(function(data){
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 router.get("/", function(req, res) {
     burger.all(function(data){
