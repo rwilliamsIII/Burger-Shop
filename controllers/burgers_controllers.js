b1cd40ccf99ae90ae9d2c032f61ce7279296a8ff
Robert Williams <robert@a3marketing.com>
2020-05-04 22:22:37 -0500

    updated post route

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,15 +5,6 @@ const router = express.Router();
 const burger = require("../model/burgers");
 
 
-router.get("*", function(req, res){
-    burger.all(function(data){
-        const hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
-});
 
 router.get("/", function(req, res) {
     burger.all(function(data){
@@ -26,10 +17,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create([
+    burger.insertOne([
         ["burger_name"], [req.body.burger_name], function(result) {
         res.json({ id: result.insertId });
-    }
+        }
     ]);
   });
 
