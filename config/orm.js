f49d69ac210245b8e1d8a6398e0aaeec7561966f
Robert Williams <robert@a3marketing.com>
2020-05-06 15:10:06 -0500

    fixed issue with orm

--- config/orm.js
+++ config/orm.js
@@ -36,7 +36,7 @@ function objToSql(ob) {
 
 
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
           if (err) {
